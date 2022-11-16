7ae6555579782d0bfcb09eb1cd14d675cb7f6846
MrDos180 <ilya180@yandex.ru>
2022-11-16 18:21:51 +0300

    add task1

---
+++ src/test/java/com/example/java_task1/Task1.java
@@ -0,0 +1,20 @@
+package com.example.java_task1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        byte Byte1 = 127;
+        byte Byte2 = 1;
+        double D1 = 100.6;
+
+        //переполнение
+        System.out.println((byte) (Byte1+Byte2));
+        //вычисления разных типов
+        System.out.println(Byte1-D1);
+
+
+
+
+
+    }
+}
